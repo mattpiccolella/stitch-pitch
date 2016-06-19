48cc638e34423ab3afa9ddb25159a2f2736bd8e3
Samantha Wiener <srwiener@fb.com>
2016-06-18 21:11:41 -0700

    Add video recording functionality

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from config import config
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('app.html')
+
+@app.route("/test.html")
+def test():
+    return render_template("test.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+	currTime = time.time()
+
+	request.files["video"].save("uploads/video-"+str(currTime)+".webm")
+	return "SUCCESS"
 
 if __name__ == '__main__':
     app.run()
